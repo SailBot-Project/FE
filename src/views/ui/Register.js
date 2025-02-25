874fc8550c3fc1b7d54472b7b1ed8ae1af1be073
ho-taek <kht072623@naver.com>
2025-02-25 17:01:09 +0900

    ui : 표 생성 - #13

--- src/views/ui/Register.js
+++ src/views/ui/Register.js
@@ -1,8 +1,29 @@
 import React, { useState } from "react";
-
+import { Card, CardBody, CardHeader, Table } from "reactstrap";
 const Register = () => {
   const [csvContent, setCsvContent] = useState("");
   const [fileName, setFileName] = useState("선택안함");
+
+  const data = [
+    {
+      id: 1,
+      date: "2024-02-01",
+      name: "곽",
+      number: "010",
+      email: "kh@naver.com",
+      category: "긍정",
+      request: "이거이거 개선해줘!",
+    },
+    {
+      id: 2,
+      date: "2024-02-01",
+      name: "곽",
+      number: "010",
+      email: "kh@naver.com",
+      category: "긍정",
+      request: "이거이거 개선해줘!",
+    },
+  ];
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -38,8 +59,37 @@ const Register = () => {
         </button>
       </div>
       <div className="w-full max-w-3xl mt-4 border rounded-md overflow-hidden">
-        <div className="bg-gray-300 px-4 py-2 font-bold">파일내용</div>
-        <div className="p-4 bg-white min-h-[200px]"></div>
+        <Card>
+          <CardHeader>파일내용</CardHeader>
+          <CardBody>
+            <Table bordered className="mt-2">
+              <thead>
+                <tr className="bg-light">
+                  <th>ID</th>
+                  <th>등록날짜</th>
+                  <th>이름</th>
+                  <th>연락처</th>
+                  <th>이메일</th>
+                  <th>긍정/부정</th>
+                  <th>개선사항</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item, index) => (
+                  <tr key={item.id} className={index === 0 ? "bg-light" : ""}>
+                    <td>{item.id}</td>
+                    <td>{item.date}</td>
+                    <td>{item.name}</td>
+                    <td>{item.number}</td>
+                    <td>{item.email}</td>
+                    <td>{item.category}</td>
+                    <td>{item.request}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </CardBody>
+        </Card>
       </div>
       <div className="d-flex justify-content-center">
         <button className="bg-button mt-2 px-4 py-2 rounded-pill">등록</button>
